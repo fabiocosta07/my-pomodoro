f018c9c87cc712bc32dc9f2fc843c67b8edf5489
fabio <fabiocosta07@gmail.com>
2021-12-21 22:21:24 -0300

    add timer countdown

--- my-pomodoro/src/components/timer/index.jsx
+++ my-pomodoro/src/components/timer/index.jsx
@@ -1,6 +1,11 @@
 import './index.css';
+import useTimer from 'easytimer-react-hook';
+
 
 export default function Timer() {
+    const [timer, isTargetAchieved] = useTimer({ countdown: true, startValues: { minutes: 25 } });
+
+    timer.start();
     return (
         <div className="timer">
             <div className="timerTab">
@@ -8,7 +13,7 @@ export default function Timer() {
                 <a className="tabButton" onclick="activateTab('Short Break')">Short Break</a>
                 <a className="tabButton" onclick="activateTab('Long Break')">Long Break</a>
             </div>
-            <label>25:00</label>
+            <label>{timer.getTimeValues().toString()}</label>
             <a className="timerButton">START</a>
         </div>
     )
